d668fe72f2a1aefc5d4ec9cab22aef2e4e284065
jmwojcikowski <jakubwojcikowski00@gmail.com>
2024-01-09 11:13:28 +0100

    Change browser route to Hash router

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes} from 'react-router-dom';
 import MainPage from './MainPage';
 import ThankYou from './ThankYou';
 
